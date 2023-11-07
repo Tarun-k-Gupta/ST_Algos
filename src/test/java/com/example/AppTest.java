010fa883657d452241eaef46c707ecab57366fd6
Tarun-k-Gupta <tarungupta_er@rediffmail.com>
2023-11-07 19:03:00 +0530

    tests

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,38 +1,72 @@
 package com.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.Assert;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+
+    @Test
+    public void addPositive() {
+        double a = 2;
+        double b = 1;
+        double expectedResult = 3;
+        Assert.assertEquals(expectedResult, Main.add(a, b), 0.001); 
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void addNegative() {
+        double a = 2;
+        double b = 1;
+        double expectedResult = 1;
+        Assert.assertNotEquals(expectedResult, Main.add(a, b), 0.001); 
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void subPositive() {
+        double a = 2;
+        double b = 1;
+        double expectedResult = 1;
+        Assert.assertEquals(expectedResult, Main.subtract(a, b), 0.001);
     }
+
+    @Test
+    public void subNegative() {
+        double a = 2;
+        double b = 1;
+        double expectedResult = 2;
+        Assert.assertNotEquals(expectedResult, Main.subtract(a, b));
+    }
+    
+
+    @Test
+    public void mulPositive() {
+        double a = 2;
+        double b = 1;
+        double expectedResult = 2;
+        Assert.assertEquals(expectedResult, Main.multiply(a, b), 0.001);
+    }
+
+    @Test
+    public void mulNegative() {
+        double a = 2;
+        double b = 1;
+        double expectedResult = 1;
+        Assert.assertNotEquals(expectedResult, Main.multiply(a, b));
+    }
+
+    @Test
+    public void dividePositive() {
+        double a = 2;
+        double b = 1;
+        double expectedResult = 2;
+        Assert.assertEquals(expectedResult, Main.divide(a, b), 0.001);
+    }
+
+    @Test
+    public void divideNegative() {
+        double a = 2;
+        double b = 1;
+        double expectedResult = 3;
+        Assert.assertNotEquals(expectedResult, Main.divide(a, b));
+    }   
 }
